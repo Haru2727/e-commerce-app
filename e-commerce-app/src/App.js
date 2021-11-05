06940179c0167d2c68d3e7f562fa189a60714fa0
Arash Saeedi <saeediarash84@gmail.com>
2021-11-04 20:52:46 -0400

    added template set it into the react app

---
+++ e-commerce-app/src/App.js
@@ -0,0 +1,156 @@
+
+
+function App() {
+  return (
+    <div class="grid-container">
+    <header class="row">
+        <div>
+            <a class="brand" href="index.html">E-Sales</a>
+        </div>
+        <div>
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+        </div>
+    </header>
+    <main>
+        <div class="row center">
+            <div class="card">
+                <a href="product.html">
+                    
+                    <img class="medium" src="./images/p1.jpg" alt="product"/>
+                </a>
+                <div class="card-body">
+                    <a href="product.html">
+                        <h2>Product for sale</h2>
+                    </a>
+                    <div class="rating">
+                        <span> <i class="fa fa-star"></i> </span>
+                        <span> <i class="fa fa-star"></i> </span>
+                        <span> <i class="fa fa-star"></i> </span>
+                        <span> <i class="fa fa-star"></i>  </span>
+                        <span> <i class="fa fa-star"></i> </span>
+                    </div>
+                    <div class="price">
+                        $200
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <a href="product.html">
+                    
+                    <img class="medium" src="./images/p1.jpg" alt="product"/>
+                </a>
+                <div class="card-body">
+                    <a href="product.html">
+                        <h2>Product for sale</h2>
+                    </a>
+                    <div class="rating">
+                        <span> <i class="fa fa-star"></i> </span>
+                        <span> <i class="fa fa-star"></i> </span>
+                        <span> <i class="fa fa-star"></i> </span>
+                        <span> <i class="fa fa-star"></i>  </span>
+                        <span> <i class="fa fa-star"></i> </span>
+                    </div>
+                    <div class="price">
+                        $200
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <a href="product.html">
+                    
+                    <img class="medium" src="./images/p1.jpg" alt="product"/>
+                </a>
+                <div class="card-body">
+                    <a href="product.html">
+                        <h2>Product for sale</h2>
+                    </a>
+                    <div class="rating">
+                        <span> <i class="fa fa-star"></i> </span>
+                        <span> <i class="fa fa-star"></i> </span>
+                        <span> <i class="fa fa-star"></i> </span>
+                        <span> <i class="fa fa-star"></i>  </span>
+                        <span> <i class="fa fa-star"></i> </span>
+                    </div>
+                    <div class="price">
+                        $200
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <a href="product.html">
+                    
+                    <img class="medium" src="./images/p1.jpg" alt="product"/>
+                </a>
+                <div class="card-body">
+                    <a href="product.html">
+                        <h2>Product for sale</h2>
+                    </a>
+                    <div class="rating">
+                        <span> <i class="fa fa-star"></i> </span>
+                        <span> <i class="fa fa-star"></i> </span>
+                        <span> <i class="fa fa-star"></i> </span>
+                        <span> <i class="fa fa-star"></i>  </span>
+                        <span> <i class="fa fa-star"></i> </span>
+                    </div>
+                    <div class="price">
+                        $200
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <a href="product.html">
+                    
+                    <img class="medium" src="./images/p1.jpg" alt="product"/>
+                </a>
+                <div class="card-body">
+                    <a href="product.html">
+                        <h2>Product for sale</h2>
+                    </a>
+                    <div class="rating">
+                        <span> <i class="fa fa-star"></i> </span>
+                        <span> <i class="fa fa-star"></i> </span>
+                        <span> <i class="fa fa-star"></i> </span>
+                        <span> <i class="fa fa-star"></i>  </span>
+                        <span> <i class="fa fa-star"></i> </span>
+                    </div>
+                    <div class="price">
+                        $200
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <a href="product.html">
+                    
+                    <img class="medium" src="./images/p1.jpg" alt="product"/>
+                </a>
+                <div class="card-body">
+                    <a href="product.html">
+                        <h2>Product for sale</h2>
+                    </a>
+                    <div class="rating">
+                        <span> <i class="fa fa-star"></i> </span>
+                        <span> <i class="fa fa-star"></i> </span>
+                        <span> <i class="fa fa-star"></i> </span>
+                        <span> <i class="fa fa-star"></i>  </span>
+                        <span> <i class="fa fa-star"></i> </span>
+                    </div>
+                    <div class="price">
+                        $200
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </main>
+    <footer class="row center">All Rights Reserved.</footer>
+</div>
+  );
+}
+
+export default App;
