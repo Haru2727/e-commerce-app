b7ad0d7a63b658829090306d7a3723f19bec087b
Arash Saeedi <saeediarash84@gmail.com>
2021-11-05 01:10:38 -0400

    made homescreen componenet but cannot get it to generate cards, will stop for the night here

--- e-commerce-app/src/App.js
+++ e-commerce-app/src/App.js
@@ -1,32 +1,34 @@
-import React from "react";
-import Product from "./components/Product";
-import data from './data';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div className="grid-container">
-    <header className="row">
-        <div>
-            <a className="brand" href="/">E-Sales</a>
-        </div>
-        <div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              E-sales
+            </a>
+          </div>
+          <div>
             <a href="/cart">Cart</a>
             <a href="/signin">Sign In</a>
-        </div>
-    </header>
-    <main>
-        <div>
-        <div className="row center">
-            {
-                data.products.map((product) => (
-                    <Product key={product._id} product={product}></Product>
-                ))
-            }
-             </div>
-             </div>
-    </main>
-    <footer className="row center">All Rights Reserved.</footer>
-    </div>
+          </div>
+       
+        </header>
+        <main>
+            <Routes>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+    
+          <Route path="/" component={HomeScreen} exact></Route>
+          </Routes>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
